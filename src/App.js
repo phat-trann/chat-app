29c12195e2e086f9a96987c22a685fd49f4932ed
phat.tran <phat.tran@globee.hk>
2022-03-30 23:57:06 +0700

    - Update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import { ChatEngine } from 'react-chat-engine';
-
-import ChatFeed from './components/ChatFeed';
-
 function App() {
   return (
-    <ChatEngine
-      height="98vh"
-      projectID="b0cc8e75-b499-4af8-b18c-308f710c12e0"
-      userName="phat_tran"
-      userSecret=""
-      renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps} />}
-    />
+    <div className="App">
+    </div>
   );
 }
 
