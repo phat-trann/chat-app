8a29ea13de1575c9cd26a4044081ec41f13ec233
phat.tran <phat.tran@globee.hk>
2022-04-01 16:46:24 +0700

    Init chat-app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Chat application using ReactJS
 - Follow https://www.youtube.com/watch?v=MJzbJQLGehs
+
+- Using
++ npm install stream-chat universal-cookie
++ npm install steam-chat-react --force
++ npm install sass
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-function App() {
+import { ChannelContainer, ChannelListContainer } from './components';
+
+const apiKey = 'xx7fy8yn4mkq';
+const client = StreamChat.getInstance(apiKey);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
